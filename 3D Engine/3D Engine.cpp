656bb5d2b5855810640e77541c2c40e3174ec803
THEGeorgie <memesport140@gmail.com>
2022-10-08 17:30:29 +0200

    The cube now works

--- 3D Engine/3D Engine.cpp
+++ 3D Engine/3D Engine.cpp
@@ -20,80 +20,88 @@ struct matrix4x4 {
 class  Engine3D : public olcConsoleGameEngine
 {
 public:
-	Engine3D() {
-		m_sAppName = L"3D rendere demo";
+	engine()
+	{
+		m_sAppName = L"3D Demo";
 	 }
 private:
 
 
+private:
+	mesh meshCube;
+	mat4x4 matProj;
+
 	float fTheta;
 
-	void MultiplyMatrixVector(vector3& i, vector3& o, matrix4x4& m) {
+	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
+	{
 		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
 		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
 		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
 		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
 
-
-		if (w != 0.0f) {
-			o.x /= w; o.y /= w; o.z = w;
+		if (w != 0.0f)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
 		}
 	}
 
-	bool OnUserCreate() override {
+public:
+	bool OnUserCreate() override
+	{
 		meshCube.tris = {
+
 			//JUG
-			{0.0f, 0.0f, 0.0f,	0.0f, 1.0f,0.0f,	1.0f,1.0f,0.0f},
-			{0.0f, 0.0f, 0.0f,	1.0f, 1.0f,0.0f,	1.0f,0.0f,0.0f},
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
 			//VZHOD
-			{1.0f, 0.0f, 0.0f,	1.0f, 1.0f,0.0f,	1.0f,1.0f,1.0f},
-			{1.0f, 0.0f, 0.0f,	1.0f, 1.0f,1.0f,	1.0f,0.0f,1.0f},
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
 			//SEVER
-			{1.0f, 0.0f, 1.0f,  1.0f, 1.0f, 1.0f,  0.0f, 1.0f, 1.0f},
-			{1.0f, 0.0f, 1.0f,  0.0f, 1.0f, 1.0f,  0.0f, 0.0f, 1.0f},
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
 			//ZAHOD
-			{0.0f, 0.0f, 1.0f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 1.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f, 0.0f},
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-			//ZGORNJI 
-			{0.0f, 1.0f, 0.0f,  0.0f, 1.0f, 1.0f,  1.0f, 1.0f, 1.0f},
-			{0.0f, 1.0f, 0.0f,  1.0f, 1.0f, 1.0f,  1.0f, 1.0f, 0.0f},
+			//ZGORNJE                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
 
-			//SPODNJI
-			{1.0f, 0.0f, 1.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, 0.0f},
-			{1.0f, 0.0f, 1.0f,  0.0f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f},
+			//SPODNJE                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
 
 		};
 
-		//PROJEKCIJA
-		float fBlizu = 0.1f;
-		float fDalec = 1000.0f;
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
 		float fFov = 90.0f;
-		float fAsR = (float)ScreenHeight() / (float)ScreenWidth();
-		float fFovRad = 1.0f / tanf(fFov *0.5f/ 180.0f * 3.14159f);
+		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
-		matrixcProj.m[0][0] = fAsR * fFovRad;
-		matrixcProj.m[1][1] = fFovRad;
-		matrixcProj.m[2][2] = fDalec / (fDalec - fBlizu);
-		matrixcProj.m[3][2] = (-fDalec * fBlizu) / (fDalec - fBlizu);
-		matrixcProj.m[2][3] = 1.0f;
-		matrixcProj.m[3][3] = 0.0f;
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar / (fFar - fNear);
+		matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+		matProj.m[2][3] = 1.0f;
+		matProj.m[3][3] = 0.0f;
 
 		return true;
 	}
 	
-
-	bool OnUserUpdate(float fElapsedTime) override{
-		
+	bool OnUserUpdate(float fElapsedTime) override
+	{
 		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
-		matrix4x4 matRotZ, matRotX;
+		mat4x4 matRotZ, matRotX;
 		fTheta += 1.0f * fElapsedTime;
 
+		//Vrtenje Z
 		matRotZ.m[0][0] = cosf(fTheta);
 		matRotZ.m[0][1] = sinf(fTheta);
 		matRotZ.m[1][0] = -sinf(fTheta);
@@ -101,6 +109,7 @@ private:
 		matRotZ.m[2][2] = 1;
 		matRotZ.m[3][3] = 1;
 
+		//Vrtenje X
 		matRotX.m[0][0] = 1;
 		matRotX.m[1][1] = cosf(fTheta * 0.5f);
 		matRotX.m[1][2] = sinf(fTheta * 0.5f);
@@ -108,57 +117,61 @@ private:
 		matRotX.m[2][2] = cosf(fTheta * 0.5f);
 		matRotX.m[3][3] = 1;
 
-		//RiSANJE TRIKOTNIKI
-		for (auto tri : meshCube.tris) {
+		//Nariši trikotnike
+		for (auto tri : meshCube.tris)
+		{
+			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
 
-			triangel triProjekcija, triPrevajen, triRotacijaZ, triRotazijaZX;
+			//Vrti po osi Z
+			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+			MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+			MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
 
-			MultiplyMatrixVector(tri.p[0], triRotacijaZ.p[0], matRotZ);
-			MultiplyMatrixVector(tri.p[1], triRotacijaZ.p[1], matRotZ);
-			MultiplyMatrixVector(tri.p[2], triRotacijaZ.p[2], matRotZ);
+			//Zavrti po osi X
+			MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+			MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
 
-			MultiplyMatrixVector(triRotacijaZ.p[0], triRotazijaZX.p[0], matRotX);
-			MultiplyMatrixVector(triRotacijaZ.p[1], triRotazijaZX.p[1], matRotX);
-			MultiplyMatrixVector(triRotacijaZ.p[2], triRotazijaZX.p[1], matRotX);
+			//Odmik v zaslon
+			triTranslated = triRotatedZX;
+			triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
 
-			triPrevajen = triRotazijaZX;
-			triPrevajen.p[0].z = triRotazijaZX.p[0].z + 3.0f;
-			triPrevajen.p[1].z = triRotazijaZX.p[1].z + 3.0f;
-			triPrevajen.p[2].z = triRotazijaZX.p[2].z + 3.0f;
+			//Projektirajte trikotnike od 3D do 2D
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
 
-			MultiplyMatrixVector(triPrevajen.p[0], triProjekcija.p[0], matrixcProj);
-			MultiplyMatrixVector(triPrevajen.p[1], triProjekcija.p[1], matrixcProj);
-			MultiplyMatrixVector(triPrevajen.p[2], triProjekcija.p[2], matrixcProj);
+			//Merilo v pogled
+			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
-
-			triProjekcija.p[0].x += 1.0f; triProjekcija.p[0].y += 1.0f;
-			triProjekcija.p[1].x += 1.0f; triProjekcija.p[1].y += 1.0f;
-			triProjekcija.p[2].x += 1.0f; triProjekcija.p[2].y += 1.0f;
-			triProjekcija.p[0].x *= 0.5f * (float)ScreenWidth();
-			triProjekcija.p[0].y *= 0.5f * (float)ScreenHeight();
-			triProjekcija.p[1].x *= 0.5f * (float)ScreenWidth();
-			triProjekcija.p[1].y *= 0.5f * (float)ScreenHeight();
-			triProjekcija.p[2].x *= 0.5f * (float)ScreenWidth();
-			triProjekcija.p[2].y *= 0.5f * (float)ScreenHeight();
-
-			DrawTriangle(triProjekcija.p[0].x, triProjekcija.p[0].y,
-				triProjekcija.p[1].x, triProjekcija.p[1].y,
-				triProjekcija.p[2].x, triProjekcija.p[2].y,
+			//Rasteriziraj trikotnik
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+				triProjected.p[1].x, triProjected.p[1].y,
+				triProjected.p[2].x, triProjected.p[2].y,
 				PIXEL_SOLID, FG_WHITE);
+
 		}
+
 
 		return true;
 	}
+
 };
 
-
-int main() {
-	Engine3D engine;
-
-	if (engine.ConstructConsole(256, 240, 4, 4)) {
-		engine.Start();
-	}
-
+int main()
+{
+	engine demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+		demo.Start();
 	return 0;
 }
-
